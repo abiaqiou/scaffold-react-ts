82cf326ae1ddafd1f4cad625d029c6592dba2665
abiaqiou <abiaqiou@gmail.com>
2021-04-03 14:30:40 +0800

    add react-refresher

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Counter from './component/counter'
 import image from './react.png'
 import logo from './react.svg'
 
@@ -9,6 +10,7 @@ const App = () => {
       <h2>process.env.NODE_ENV: {process.env.NODE_ENV}</h2>
       <h2>process.env.myEnv: {process.env.myEnv}</h2>
       <img src={logo} width="200" />
+      <Counter />
     </>
   )
 }
---
+++ src/component/counter/index.tsx
@@ -0,0 +1,14 @@
+import react, { useState } from 'react'
+
+const Counter = () => {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h2>{count}</h2>
+      <button onClick={() => setCount((count) => count + 1)}>add</button>
+    </>
+  )
+}
+
+export default Counter
