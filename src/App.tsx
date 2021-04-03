55a8e1af9c11369cc8bfa1ecd067c240d14fdc5a
abiaqiou <abiaqiou@gmail.com>
2021-04-03 15:29:55 +0800

    add eslint and lint code

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,10 @@ const App = () => {
   return (
     <>
       <h1>Scaffold-react-ts</h1>
-      <img src={image} width="400" />
+      <img src={image} width="400" alt="react" />
       <h2>process.env.NODE_ENV: {process.env.NODE_ENV}</h2>
       <h2>process.env.myEnv: {process.env.myEnv}</h2>
-      <img src={logo} width="200" />
+      <img src={logo} width="200" alt="react" />
       <Counter />
     </>
   )
--- src/component/counter/index.tsx
+++ src/component/counter/index.tsx
@@ -1,4 +1,4 @@
-import react, { useState } from 'react'
+import { useState } from 'react'
 
 const Counter = () => {
   const [count, setCount] = useState(0)
