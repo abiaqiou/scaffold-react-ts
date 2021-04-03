5d308a11b5a3a115aa63a0df8d1217ef79f23376
abiaqiou <abiaqiou@gmail.com>
2021-04-03 13:53:31 +0800

    add asset/inline

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import image from './react.png'
+import logo from './react.svg'
 
 const App = () => {
   return (
     <>
       <h1>Scaffold-react-ts</h1>
       <img src={image} width="400" />
+      <img src={logo} width="200" />
       <svg></svg>
     </>
   )
