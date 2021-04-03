a5357c858bbaeb9cec5166fe7ba73bd5f857b601
abiaqiou <abiaqiou@gmail.com>
2021-04-03 13:42:50 +0800

    add asset/resource

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import image from './react.png'
+
 const App = () => {
-  return <h1>Scaffold-react-ts</h1>
+  return (
+    <>
+      <h1>Scaffold-react-ts</h1>
+      <img src={image} width="400" />
+      <svg></svg>
+    </>
+  )
 }
 
 export default App
